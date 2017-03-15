c62e1674743dd503fe0accaa0d3402b5d025d6a7
Reuben Cleetus <rcleetus@gmail.com>
2017-03-15 11:52:38 -0400

    First commit: VNet with 3 Subnets

---
+++ Deploy-SimpleIaasTemplate.ps1
@@ -0,0 +1,26 @@
+﻿### Define variables
+{
+$location = 'Australia Southeast'
+$resourceGroupName = 'rc-simple-iaas-template'
+$resourceDeploymentName = 'rc-arm-iaas-template-deployment'
+$templatePath = "\\tsclient\azure-share\azure-resource-manager-deep-dive\"
+$templateFile = 'rc_simpleIaas.json'
+$template = $templatePath + '\' + $templateFile
+}
+
+### Create Resource Group
+{
+New-AzureRmResourceGroup `
+    -Name $resourceGroupName `
+    -Location $Location `
+    -Verbose -Force
+}
+
+### Deploy Resources
+{
+New-AzureRmResourceGroupDeployment `
+    -Name $resourceDeploymentName `
+    -ResourceGroupName $resourceGroupName `
+    -TemplateFile $template `
+    -Verbose -Force
+}
