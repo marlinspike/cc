86cf207c5da6f34cac8e24f2a1d7290884a59fea
Reuben Cleetus <rcleetus@gmail.com>
2017-03-15 13:00:53 -0400

    Added Bastion Host

--- Deploy-SimpleIaasTemplate.ps1
+++ Deploy-SimpleIaasTemplate.ps1
@@ -1,11 +1,13 @@
 ﻿### Define variables
 {
 $location = 'Australia Southeast'
-$resourceGroupName = 'rc-simple-iaas-template'
-$resourceDeploymentName = 'rc-arm-iaas-template-deployment'
-$templatePath = "\\tsclient\azure-share\azure-resource-manager-deep-dive\"
+$resourceGroupName = 'ccnet_resources_new'
+$resourceDeploymentName = 'ccnet'
+$templatePath = "\\tsclient\azure-share\CloudConcierge-ARM-Template"
 $templateFile = 'rc_simpleIaas.json'
 $template = $templatePath + '\' + $templateFile
+$vm_jumpbox_password = "Pres_jumpbox_1234#"
+$vm_jumpbox_secure_password = $vm_jumpbox_password | ConvertTo-SecureString -AsPlainText -Force
 }
 
 ### Create Resource Group
@@ -18,9 +20,13 @@ New-AzureRmResourceGroup `
 
 ### Deploy Resources
 {
+$additionalParameters = New-Object -TypeName Hashtable
+$additionalParameters['vm_jumpboxAdminPassword'] = $vm_jumpbox_secure_password
+
 New-AzureRmResourceGroupDeployment `
     -Name $resourceDeploymentName `
     -ResourceGroupName $resourceGroupName `
     -TemplateFile $template `
+	@additionalParameters ` 
     -Verbose -Force
 }
